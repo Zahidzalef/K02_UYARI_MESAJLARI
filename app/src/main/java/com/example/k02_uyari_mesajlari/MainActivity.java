d1f8aaa5de4db104837c84f255693dc411c7d460
Zahid İyigökler <iyigoklerzahid55@gmail.com>
2025-06-02 16:14:52 +0300

    Files added.

---
+++ app/src/main/java/com/example/k02_uyari_mesajlari/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.k02_uyari_mesajlari;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    // Todo: Açıklayıcı metin ekle.
+    public void save(View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // save
+                Toast.makeText(getApplicationContext(),"Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // don't save
+                Toast.makeText(MainActivity.this,"Not Saved",Toast.LENGTH_LONG).show();
+            }
+        });
+        alert.show();
+    }
+}
